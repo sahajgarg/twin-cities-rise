2d0b7a21f1b15466bbe7d06fd6fb608c015c4578
raguiar2 <raguiar2@stanford.edu>
2017-04-16 16:40:55 -0700

    adding first comments- still testing VCS integration

--- TwinCitiesRise2/app/src/main/java/a50/cs/twincitiesrise/MainActivity.java
+++ TwinCitiesRise2/app/src/main/java/a50/cs/twincitiesrise/MainActivity.java
@@ -3,6 +3,7 @@ package a50.cs.twincitiesrise;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//This is a test comment! please remove in future commits
 public class MainActivity extends AppCompatActivity {
 
     @Override
