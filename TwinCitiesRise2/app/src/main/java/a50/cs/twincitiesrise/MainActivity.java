711fb77cf9737feaf103c24e0d67996fcbf7156f
Grace Hong <ghong97@stanford.edu>
2017-04-15 20:11:11 -0700

    created blank android project

---
+++ TwinCitiesRise2/app/src/main/java/a50/cs/twincitiesrise/MainActivity.java
@@ -0,0 +1,13 @@
+package a50.cs.twincitiesrise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
